29f5abf385444f7aa8bef956e9f7aca7565ca50e
matajan <91990671+matajan@users.noreply.github.com>
2024-03-04 15:41:45 +0100

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,36 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import sqlite3
+import datetime
+
+
+
+con = sqlite3.connect("ips.db")
+cur = con.cursor()
+try:
+	cur.execute("create table t_ips (time timestamp primary key, r_ip text)")                        
+except sqlite3.OperationalError:
+	print("Error creating DB!")                    
+
+
+class RequestHandler(BaseHTTPRequestHandler):
+	def log_message(self, format, *args):
+		pass
+
+	def do_GET(self):
+		ip = self.client_address[0]
+		reversed_ip = '.'.join(reversed(ip.split('.')))
+		self.send_response(200)
+		self.send_header('Content-type', 'text/plain')
+		self.end_headers()
+		print(reversed_ip)
+
+		t = datetime.datetime.now()
+		cur.execute("insert into t_ips (time, r_ip) values (?, ?)", (t, reversed_ip))
+		con.commit()
+		return
+
+if __name__ == '__main__':
+	server_address = ('', 8000)
+	httpd = HTTPServer(server_address, RequestHandler)
+	httpd.serve_forever()
+	con.close()
